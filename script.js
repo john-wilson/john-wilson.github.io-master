364c07f49b3253501dfdfa7c95f702e6f7d27085
john-wilson <jwilson18@it-mmc-m08.eastern.ewu.edu>
2016-03-18 11:11:54 -0700

    fixing js file names

---
+++ script.js
@@ -0,0 +1,39 @@
+var main = function() {
+
+    //========== START OF NAVBAR ============//
+    $('#navicon').click(function() {
+        $('.nav').css({
+            'transform': 'translateX(0)'
+        });
+    });
+
+    $('.nav').click(function() {
+        $('.nav').css({
+            'transform': 'translateX(-400px)'
+        });
+    });
+
+    
+
+    function _exit() {
+        $('.nav').css({
+            'transform': 'translateX(-200px)'
+        });
+    };
+    //========== END OF NAVBAR ============//
+    //========== START OF TO-TOP ============//
+    $('button').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, "slow");
+    });
+    //========== END OF TO-TOP ============//
+
+$(document).ready(function(){
+  $('.slider').slick({
+    settingName: setting-value
+  });
+});
+};
+
+$(document).ready(main);
